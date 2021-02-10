437167a297dc02ee71e28f87181fea52419680b2
Pierre <pierre.darcas@lacatholille.fr>
2021-02-10 15:18:39 +0100

    💚 debug CI 2

--- src/main/java/com/theater/app/security/SecurityConfig.java
+++ src/main/java/com/theater/app/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${auth0.audience}")
     private String audience;
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    @Value("${spring.security.oauth2.resourceserver.issuer-uri: https://dev-dnddcikx.eu.auth0.com/}")
     private String issuer;
 
     @Bean
